4d14ae134016d51ffc79f5f8b9b5455891f03ab9
Loris Berthod <lorisberthod@gmail.com>
2023-10-04 13:39:37 +0200

    articles instanciés

--- GesStock/src/main/java/com/mycompany/gesstock/StockProgram.java
+++ GesStock/src/main/java/com/mycompany/gesstock/StockProgram.java
@@ -11,13 +11,11 @@ public class StockProgram
 {
     public static void main(String[] args)
     {
-        /// Instancier 5 articles
-        
-        Menu menu = new Menu();
-        
-        System.out.println(menu.afficherMenuPrincipal());
-        
-        
+        Article article1 = new Article("art1","Whisky" ,29.99f , 12);
+        Article article2 = new Article("art2","Rosé" ,9f , 26);
+        Article article3 = new Article("art3","Vin blanc" ,15 , 50);
+        Article article4 = new Article("art4","Vin rouge" ,19.99f , 5);
+        Article article5 = new Article("art5","Prosecco" ,5 , 0);
     }
     
     
